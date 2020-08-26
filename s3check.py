d277196fa7453eea3fbcb2d4089b67aba56bcdb8
gbroiles <gbroiles@gmail.com>
2020-08-26 15:27:53 -0700

    add multiprocessing

--- s3check.py
+++ s3check.py
@@ -1,26 +1,33 @@
 import requests
 import sys
 import tqdm
+import multiprocessing
+
+def checkurl(target):
+    url = URLBASE + target + "/"
+#    print("Working on {}".format(url))
+    r = aws.get(url)
+    if r.status_code == 200:
+        return(target)
+    else:
+        return
 
 URLBASE = "https://s3.amazonaws.com/"
 
 filename = sys.argv[1]
+procs = int(sys.argv[2])
 
 aws = requests.Session()
 
-with open(filename,"r") as f:
-    buckets = [line.strip() for line in f]
+q = multiprocessing.Queue()
 
-working_buckets = []
+if __name__ == '__main__':
+    with open(filename,"r") as f:
+        buckets = [line.strip() for line in f]
 
-for bucket in tqdm.tqdm(buckets):
-    url = URLBASE + bucket + "/"
-    print("Working on {}".format(url))
-    r = aws.get(url)
-#    print(r.status_code)
-    if r.status_code == 200:
-        working_buckets.append(bucket)
-
-print("Working buckets:")
-for bucket in working_buckets:
-    print(bucket)
+    pool = multiprocessing.Pool(processes=procs)
+    results = [pool.apply_async(checkurl, args=(x,)) for x in buckets]
+    output = [p.get() for p in results]
+    for i in output:
+        if i:
+            print(i)
