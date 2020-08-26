f390511c9bdc7ecf92d610a9c497c9790e39f6d3
gbroiles <gbroiles@gmail.com>
2020-08-26 15:39:49 -0700

    remove unneeded queue, improve None removal from result list

--- s3check.py
+++ s3check.py
@@ -1,11 +1,9 @@
 import requests
 import sys
-import tqdm
 import multiprocessing
 
 def checkurl(target):
     url = URLBASE + target + "/"
-#    print("Working on {}".format(url))
     r = aws.get(url)
     if r.status_code == 200:
         return(target)
@@ -19,15 +17,12 @@ procs = int(sys.argv[2])
 
 aws = requests.Session()
 
-q = multiprocessing.Queue()
-
 if __name__ == '__main__':
     with open(filename,"r") as f:
         buckets = [line.strip() for line in f]
-
     pool = multiprocessing.Pool(processes=procs)
     results = [pool.apply_async(checkurl, args=(x,)) for x in buckets]
     output = [p.get() for p in results]
+    output = [i for i in output if i]
     for i in output:
-        if i:
-            print(i)
+        print(i)
